b9f17572926e0e684847a843c3110de1d295c253
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-04 16:05:07 +0100

    01-pali

--- 01-pali/script.js
+++ 01-pali/script.js
@@ -1,2 +1,27 @@
 /* Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
+
+// Chiedere all’utente di inserire una parola
+const Word = isPalindroma(prompt("inserisci una parola"))
+
+// Stampare il risultato
+console.log(Word);
+
+// Funzione che inverte la parola inserita
+ function isPalindroma(Word) {
+     let isPalindroma = "";
+     for(let i = Word.length - 1; i >=0; i--) {
+         isPalindroma = isPalindroma + Word[i];
+     }
+     return isPalindroma;
+ }
+
+// Controllo che la parola inserita sia uguale a quella inversa
+ if (isPalindroma(Word) === Word) {
+           console.log('La parola è palidroma')
+       } else {
+        console.log('La parola non è palindroma')
+       }
+    
+     
+ 
